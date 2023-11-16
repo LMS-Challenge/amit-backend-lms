36a58d9190247fe105ba7811b6149dc535195c60
mark ramy <markramyy@gmail.com>
2023-11-16 17:38:20 +0200

    Applying the new classes to the admin file

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import course
+from .models import course, Content, Assignment, Quiz
 
 # Register your models here.
 admin.site.register(course)
+admin.site.register(Content)
+admin.site.register(Assignment)
+admin.site.register(Quiz)
+
