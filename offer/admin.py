45bb79b5999773e9eda880d3d75c844e52ef28d6
mark ramy <markramyy@gmail.com>
2023-11-19 11:33:07 +0200

    Handling the Discounted price of the course so that it is null when no new price checked and does not have negative value

--- offer/admin.py
+++ offer/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+
 from .models import offer, course
+
 
 @admin.register(offer)
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ('course', 'original_price', 'discount', 'new_price', 'start_date', 'end_date', 'status')
-    readonly_fields = ('original_price',)
+    list_display = ('course', 'original_price', 'discounted_price', 'new_price', 'start_date', 'end_date', 'status')
+    readonly_fields = ('original_price', 'discounted_price',)
 
     def original_price(self, obj):
         # Assuming there is a foreign key from Offer to Course named 'course'
         return f"${obj.course.course_price}"
-    original_price.short_description = "Original Price"  # Optional: To set a header column name
 
+    def discounted_price(self, obj):
+        # Assuming there is a foreign key from Offer to Course named 'course'
+        return f"${obj.course.course_price - obj.discount}" if obj.new_price else "N/A"
+
+    def save_model(self, request, obj, form, change):
+        obj.full_clean()  # This will call the clean method
+        super().save_model(request, obj, form, change)
