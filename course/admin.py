0acf92f5729ebd2759cf309e1914135d93fd5759
mark ramy <markramyy@gmail.com>
2023-11-19 15:57:51 +0200

    Editing the admin views and Adding filters to it

--- course/admin.py
+++ course/admin.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 from .models import course, Content, Assignment, Quiz
 
 # Register your models here.
-admin.site.register(course)
-admin.site.register(Content)
-admin.site.register(Assignment)
-admin.site.register(Quiz)
+@admin.register(course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_name', 'category', 'course_price', 'certificate', 'rating',)
+    list_filter = ('category', 'course_price' ,'certificate', 'rating',)
 
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('course', 'content', 'content_description',)
+
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('course', 'title', 'due_date',)
+    list_filter = ('due_date',)
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('course', 'quiz_title','quiz_deadline',)
+    list_filter = ('quiz_deadline',)
