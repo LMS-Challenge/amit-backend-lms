4cd7e67012a6ca8d280bbf82a21c2f15664558f7
mark ramy <markramyy@gmail.com>
2023-11-19 10:50:17 +0200

    Adding the original price of the course to the offer section

--- offer/admin.py
+++ offer/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import offer
+from .models import offer, course
 
-# Register your models here.
-admin.site.register(offer)
+@admin.register(offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('course', 'original_price', 'discount', 'new_price', 'start_date', 'end_date', 'status')
+    readonly_fields = ('original_price',)
+
+    def original_price(self, obj):
+        # Assuming there is a foreign key from Offer to Course named 'course'
+        return f"${obj.course.course_price}"
+    original_price.short_description = "Original Price"  # Optional: To set a header column name
+
